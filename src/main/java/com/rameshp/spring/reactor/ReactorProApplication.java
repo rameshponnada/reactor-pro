c3764a4a599bd5a2410c5050095e8136c5d7799a
Ramesh Ponnada <rameshponnada@gmail.com>
2020-01-04 14:19:06 +0530

    Initial Commit

---
+++ src/main/java/com/rameshp/spring/reactor/ReactorProApplication.java
@@ -0,0 +1,13 @@
+package com.rameshp.spring.reactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorProApplication.class, args);
+	}
+
+}
